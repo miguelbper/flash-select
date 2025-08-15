87e26f11c163777610f73fc9d756fd1cd44d7dbc
Miguel Pereira <miguel.b.per@gmail.com>
2025-08-15 23:44:06 +0100

    Add shell script to run all benchmarks

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,5 +1,6 @@
-uv run benchmark/benchmark.py --m_train 1000  --m_val 1000  --n 10  --plot_results
-uv run benchmark/benchmark.py --m_train 5000  --m_val 5000  --n 50  --plot_results
-uv run benchmark/benchmark.py --m_train 10000 --m_val 10000 --n 100 --plot_results
-uv run benchmark/benchmark.py --m_train 25000 --m_val 25000 --n 250 --plot_results
-uv run benchmark/benchmark.py --m_train 50000 --m_val 50000 --n 500 --plot_results
+uv run benchmark/benchmark.py --m_val 10000  --n 10  --n_estimators 100 --plot_results | tee logs/benchmark_10000_10.log
+uv run benchmark/benchmark.py --m_val 25000  --n 25  --n_estimators 100 --plot_results | tee logs/benchmark_25000_25.log
+uv run benchmark/benchmark.py --m_val 50000  --n 50  --n_estimators 100 --plot_results | tee logs/benchmark_50000_50.log
+uv run benchmark/benchmark.py --m_val 75000  --n 75  --n_estimators 100 --plot_results | tee logs/benchmark_75000_75.log
+uv run benchmark/benchmark.py --m_val 100000 --n 100 --n_estimators 100 --plot_results | tee logs/benchmark_100000_100.log
+uv run benchmark/benchmark.py --m_val 200000 --n 200 --n_estimators 100 --plot_results | tee logs/benchmark_200000_200.log
