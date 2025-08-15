b2767ccbd1f8308a12a8d43a47e4411c75c03576
Miguel Pereira <miguel.b.per@gmail.com>
2025-08-15 10:35:17 +0100

    Use prints instead of logger

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,8 +1,6 @@
-import logging
 from time import time
 from typing import Any
 
-import colorlog
 import numpy as np
 import pandas as pd
 from numpy.typing import NDArray
@@ -11,33 +9,6 @@ from xgboost import XGBRegressor
 
 from flash_select.flash_select import FEATURE_NAME, SELECTED, T_VALUE, flash_select
 
-
-def get_logger() -> logging.Logger:
-    formatter = colorlog.ColoredFormatter(
-        "[%(cyan)s%(asctime)s%(reset)s][%(blue)s%(name)s%(reset)s][%(log_color)s%(levelname)s%(reset)s] - %(message)s",
-        log_colors={
-            "DEBUG": "purple",
-            "INFO": "green",
-            "WARNING": "yellow",
-            "ERROR": "red",
-            "CRITICAL": "red",
-        },
-        reset=True,
-        secondary_log_colors={},
-        style="%",
-    )
-
-    handler = colorlog.StreamHandler()
-    handler.setFormatter(formatter)
-
-    logging.getLogger().setLevel(logging.INFO)
-    logging.getLogger().handlers.clear()
-    logging.getLogger().addHandler(handler)
-
-    return logging.getLogger(__name__)
-
-
-log = get_logger()
 rng = np.random.default_rng(42)
 
 
@@ -83,32 +54,32 @@ def shap_select_regression(
 
 
 def benchmark(m: int, n: int, alpha: float) -> None:
-    log.info(f"Fitting xgboost model with {n} features and {m} samples")
+    print(f"Fitting xgboost model with {n} features and {m} samples")
     tree_model = get_model(m, n)
 
     X = rng.normal(size=(m, n))
     y = get_y(X)
     features = [f"feature_{i}" for i in range(n)]
 
-    log.info("Running flash_select...")
+    print("Running flash_select...")
     t0 = time()
     df_flash = flash_select(tree_model, X, y, features)
     t_flash = time() - t0
-    log.info(f"flash_select took {t_flash} seconds")
+    print(f"flash_select took {t_flash} seconds")
 
-    log.info("Running shap_select...")
+    print("Running shap_select...")
     t0 = time()
     df_shap = shap_select_regression(tree_model, X, y, features, alpha=alpha)
     t_shap = time() - t0
-    log.info(f"shap_select took {t_shap} seconds")
+    print(f"shap_select took {t_shap} seconds")
 
     speedup = t_shap / t_flash
-    log.info(f"Speedup: {speedup}")
+    print(f"Speedup: {speedup}")
 
     n_xgboost = len(tree_model.get_booster().get_score().keys())
     n_flash = (df_flash[SELECTED] == 1).sum()
     equal_selected = df_flash[SELECTED].equals(df_shap[SELECTED])
-    log.info(f"Same set of selected features? {'yes' if equal_selected else 'no'}")
+    print(f"Same set of selected features? {'yes' if equal_selected else 'no'}")
 
     df = pd.DataFrame(
         {
@@ -132,12 +103,12 @@ def main() -> None:
     for i in range(5, 9):
         n = 2**i
         m = 100 * n
-        log.info(f"Running benchmark for n = {n} features and m = {m} samples")
+        print(f"Running benchmark for n = {n} features and m = {m} samples")
         df = benchmark(m, n, alpha=1e-6)
         dfs.append(df)
 
     df = pd.concat(dfs)
-    log.info("Results:\n" + df.to_string())
+    print("Results:\n" + df.to_string())
 
 
 if __name__ == "__main__":
